84eabda9c5d5433d7981418588533f37accaba24
Br <br@Brs-MacBook-Pro.lan>
2021-11-22 16:32:00 -0800

    update stripe integration and updated title and icon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import {
   selectCartItems,
@@ -32,9 +33,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL : ${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments
+      <br />
+      5555555555554444 or 4242424242424242
     </div>
+
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
